38f574a8995bdc69a4e4edf408a290caa437ae3b
Jan Ulrichts <jan.ulrichts@gmail.com>
2023-08-18 19:21:01 +0200

    Copied Benchmark running code from mtreit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Test;
+
+#if RELEASE
+    BenchmarkRunner.Run<Benchmark>();
+#else
+    Benchmark b = new();
+    b.GlobalSetup();
+#endif
